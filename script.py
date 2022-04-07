92cdb24af4e9312ac4d553706ccd0ae2388bd248
yanirbuznah <65356872+yanirbuznah@users.noreply.github.com>
2022-04-07 21:37:47 +0300

    add script.py

---
+++ script.py
@@ -0,0 +1,12 @@
+import subprocess
+
+
+#HMM Tagger
+subprocess.check_call(["python", "HMM Tagger/MLETrain.py", "ass1data/data/ass1-tagger-train", "q.mle" , "e.mle"])
+subprocess.check_call(["python", "HMM Tagger/GreedyTag.py", "ass1data/data/ass1-tagger-dev-input", "q.mle" , "e.mle","output_greedy","ass1data/data/ass1-tagger-dev"])
+subprocess.check_call(["python", "HMM Tagger/HMMTag.py", "ass1data/data/ass1-tagger-dev-input", "q.mle" , "e.mle","output_hmm","ass1data/data/ass1-tagger-dev"])
+
+#Local Tagger
+subprocess.check_call(["python", "Local Feature-based Tagger/ExtractFeatures.py", "ass1data/data/ass1-tagger-train", "fetures_file"])
+subprocess.check_call(["python", "Local Feature-based Tagger/TrainModel.py", "fetures_file", "model_file"])
+subprocess.check_call(["python", "Local Feature-based Tagger/FeaturesTagger.py", "ass1data/data/ass1-tagger-dev-input", "model_file", "feature_map_file", "output"])
